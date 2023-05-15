0b64fb231e12eb0a1f4f5f4362d85e7c9fc74565
David Borland <borland@renci.org>
2023-05-15 15:13:59 -0400

    Move tiff loader to separate branch

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -2,5 +2,3 @@ export * from './web-loader';
 export * from './web-meta-data-provider';
 export * from './nifti-loader';
 export * from './nifti-meta-data-provider';
-export * from './tiff-loader';
-export * from './tiff-meta-data-provider';
