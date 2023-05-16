8f6e5f6a72d59f1d10cd160694d3e8169a856cce
David Borland <borland@renci.org>
2023-05-16 16:06:12 -0400

    Added lots of code from cornerstone example

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import { NiftiWrapper } from 'slice-view';
+import { useCornerstone } from 'hooks/use-cornerstone';
 
 const niftiUrl = `${ process.env.PUBLIC_URL }/data/test_image.nii`;
 
 export const App = () => {
+  //const initialized = useCornerstone();
+  
   return (
     <div>
-      <NiftiWrapper url={ niftiUrl } />
+      { /*initialized && */ <NiftiWrapper url={ niftiUrl } /> }
     </div>
   );
 };
--- src/slice-view/nifti-wrapper.js
+++ src/slice-view/nifti-wrapper.js
@@ -1,42 +1,86 @@
 import { useEffect, useState, useRef } from 'react';
-import { init, RenderingEngine } from '@cornerstonejs/core';
+import { RenderingEngine } from '@cornerstonejs/core';
 import { ViewportType } from '@cornerstonejs/core/dist/esm/enums';
+import { ToolGroupManager, Enums, StackScrollMouseWheelTool, ZoomTool } from '@cornerstonejs/tools';
 import { registerNiftiImageLoader, loadNiftiImage } from 'loaders';
+
+
+import * as cornerstone from '@cornerstonejs/core';
+import * as tools from '@cornerstonejs/tools';
 
 export const NiftiWrapper = ({ url }) => {
   const [viewport, setViewport] = useState();
-  const div =  useRef();
+  const div = useRef();
+  const toolGroup = useRef();
 
   // Initialize
   useEffect(() => {
-    const initialize = async () => {
-      if (!viewport) {
-        await init();
+const init = async () => {
+    const toolGroupId = 'niftiToolGroup';
+    if (!viewport) {
 
-        registerNiftiImageLoader();
 
-        const renderingEngineId = 'nifti-engine';
-        const renderingEngine = new RenderingEngine(renderingEngineId);
-    
-        const viewportId = 'nifti-viewport';
-        const viewportInput = {
-          viewportId: viewportId,
-          element: div.current,
-          type: ViewportType.STACK
-        };
+      await cornerstone.init();
 
-        renderingEngine.setViewports([viewportInput]);
-        setViewport(renderingEngine.getStackViewports()[0]);
+      tools.init();
+      tools.addTool(tools.StackScrollMouseWheelTool);
+      tools.addTool(tools.ZoomTool);
+
+
+
+
+
+
+      registerNiftiImageLoader();
+
+      const renderingEngineId = 'nifti-engine';
+      const renderingEngine = new RenderingEngine(renderingEngineId);
+  
+      const viewportId = 'nifti-viewport';
+      const viewportInput = {
+        viewportId: viewportId,
+        element: div.current,
+        type: ViewportType.STACK
+      };
+
+      renderingEngine.setViewports([viewportInput]);
+      setViewport(renderingEngine.getStackViewports()[0]);
+
+      
+      if (!toolGroup.current) {
+        toolGroup.current = ToolGroupManager.createToolGroup(toolGroupId);
+  
+        
+        toolGroup.current.addTool(StackScrollMouseWheelTool.toolName);
+        toolGroup.current.addViewport(viewportId, renderingEngineId);
+
+        toolGroup.current.setToolActive(StackScrollMouseWheelTool.toolName);
+        
+        
+
+        toolGroup.current.addTool(ZoomTool.toolName);
+        toolGroup.current.addViewport(viewportId, renderingEngineId);
+
+        toolGroup.current.setToolActive(ZoomTool.toolName, { bindings: [{ mouseButton: Enums.MouseBindings.Primary }]});
+
+        console.log("LDSKNF")
+        
       }
-    };
+    }
+  };
 
-    initialize();
+init();
+
+    return () => {
+      //ToolGroupManager.destroyToolGroup(toolGroupId);
+      //toolGroup.current = null;
+    };
   }, [viewport]);
 
   // Load image
   useEffect(() => {
     const loadImage = async url => {
-      const imageIds = await loadNiftiImage(`nifti:${ url }`);
+      const imageIds = await loadNiftiImage(`nifti:${ url }#z`);
       viewport.setStack(imageIds);
     }
 
