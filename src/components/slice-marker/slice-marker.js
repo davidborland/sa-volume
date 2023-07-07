7081bf2b72e0d9a01e20045e0205a127bac81a42
David Borland <borland@renci.org>
2023-07-07 14:45:25 -0400

    Check for number of slices

--- src/components/slice-marker/slice-marker.js
+++ src/components/slice-marker/slice-marker.js
@@ -1,24 +1,33 @@
 export const SliceMarker = ({ numImages, slice }) => {
   return (
-    <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        gap: 5
-      }}
-    >
-      <label>Slice</label>
-      <div style={{ marginTop: -5 }}>
-        { Array(numImages).fill().map((_, i) => (
-          <span 
-            key={ i }
-            style={{ 
-              fontWeight: 'bold', 
-              opacity: i === slice ? 1 : 0.25 
-            }}
-          >|</span>
-        ))}
-      </div>
-      <label>{ slice + 1 }</label>      
-    </div>
+    <>
+      { numImages === 0 ? 
+        null
+      : numImages === 1 ?
+        <div />
+      :
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            gap: 5
+          }}
+        >
+          <label>Slice</label>
+          <div style={{ marginTop: -3 }}>
+            { Array(numImages).fill().map((_, i) => (
+              <span 
+                key={ i }
+                className='small'
+                style={{ 
+                  fontWeight: 'bold', 
+                  opacity: i === slice ? 1 : 0.25 
+                }}
+              >|</span>
+            ))}
+          </div>
+          <label>{ slice + 1 } / { numImages }</label>      
+        </div>
+      }
+    </>
   );
 };
